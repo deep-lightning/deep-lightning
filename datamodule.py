b1a076785904b71887d20be59890a81246b969fd
Julio Morero <julio@tryolabs.com>
2021-11-07 20:31:44 -0300

    Fixed multiple regex as they were matching more examples than expected

--- datamodule.py
+++ datamodule.py
@@ -15,42 +15,49 @@ class DataModule(LightningDataModule):
     def setup(self, stage=None):
         train_regex = val_regex = test_regex = ".*"
 
+        train_nums = "([0-9]?[0-9]?[0-9]|1[0-1][0-9][0-9])"
+        val_nums = "(1[2-5][0-9][0-9])"
+        test_nums = "(1[6-9][0-9][0-9])"
+        light = "(up|down|back|left|right)"
+
         if self.data_regex == "vanilla":
-            train_regex = "(cube|sphere|dragon)_.*"
-            val_regex = "bunny_.*"
-            test_regex = "buddha_.*"
+            train_regex = "^(cube|sphere|dragon)_.*$"
+            val_regex = "^bunny_.*$"
+            test_regex = "^buddha_.*$"
 
         if self.data_regex == "positions":
-            train_regex = "(bunny|buddha|cube|dragon|sphere)_([0-9]?[0-9]?[0-9]|1[0-1][0-9][0-9]).*"
-            val_regex = "(bunny|buddha|cube|dragon|sphere)_1[2-5][0-9][0-9].*"
-            test_regex = "(bunny|buddha|cube|dragon|sphere)_1[6-9][0-9][0-9].*"
+            train_regex = f"^(bunny|buddha|cube|dragon|sphere)_{train_nums}(_{light})?$"
+            val_regex = f"^(bunny|buddha|cube|dragon|sphere)_{val_nums}(_{light})?$"
+            test_regex = f"^(bunny|buddha|cube|dragon|sphere)_{test_nums}(_{light})?$"
 
         # multiple lights
         if self.data_regex == "lights":
-            train_regex = "(bunny|buddha)_([0-9]?[0-9]?[0-9]|1[0-1][0-9][0-9]).*"
-            val_regex = "(bunny|buddha)_1[2-5][0-9][0-9].*"
-            test_regex = "(bunny|buddha)_1[6-9][0-9][0-9].*"
+            train_regex = f"^(bunny|buddha)_{train_nums}(_{light})?$"
+            val_regex = f"^(bunny|buddha)_{val_nums}(_{light})?$"
+            test_regex = f"^(bunny|buddha)_{test_nums}(_{light})?$"
 
         # cube and sphere
         if self.data_regex == "objects":
-            train_regex = "cube_sphere_([0-9]?[0-9]?[0-9]|1[0-1][0-9][0-9])_.*"
-            val_regex = "cube_sphere_1[2-5][0-9][0-9]_.*"
-            test_regex = "cube_sphere_1[6-9][0-9][0-9]_.*"
+            train_regex = f"^cube_sphere_{train_nums}_(horizontal|vertical)(_{light})?$"
+            val_regex = f"^cube_sphere_{val_nums}_(horizontal|vertical)(_{light})?$"
+            test_regex = f"^cube_sphere_{test_nums}_(horizontal|vertical)(_{light})?$"
 
         # multiple cameras
         if self.data_regex == "cameras":
-            train_regex = "(bunny|buddha)_([0-9]?[0-9]?[0-9]|1[0-1][0-9][0-9]).*"
-            val_regex = "(bunny|buddha)_1[2-5][0-9][0-9].*"
-            test_regex = "(bunny|buddha)_1[6-9][0-9][0-9].*"
+            train_regex = f"^(bunny|buddha)_{train_nums}(_{light})?$"
+            val_regex = f"^(bunny|buddha)_{val_nums}(_{light})?$"
+            test_regex = f"^(bunny|buddha)_{test_nums}(_{light})?$"
 
         # Assign train/val datasets for use in dataloaders
         if stage == "fit" or stage is None:
             self.train_dataset = DataLoaderHelper(self.data_dir, True, train_regex)
             self.val_dataset = DataLoaderHelper(self.data_dir, False, val_regex)
+            print(len(self.train_dataset), len(self.val_dataset))
 
         # Assign test dataset for use in dataloader
         if stage == "test" or stage is None:
             self.test_dataset = DataLoaderHelper(self.data_dir, False, test_regex)
+            print(len(self.test_dataset))
 
     def train_dataloader(self):
         return DataLoader(
